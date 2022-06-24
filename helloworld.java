c52b29a716e01638f0be5679584b820d0a1bde41
LAXMI KANT AZAD <37404280+laxmikant16d@users.noreply.github.com>
2022-06-24 02:53:18 -0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 23rd june @02:42AM (EST) triggered by webhook");
+    System.out.println("Hello World 24th june @02:42AM (EST) triggered by webhook");
     }
 }
